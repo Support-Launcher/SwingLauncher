b2a3701d40a1208cdd4968e0a25d96f6e48478e4
Cakihorse <91017164+cakihorse@users.noreply.github.com>
2023-08-20 22:34:07 +0200

    Add WebView for auth with ms

--- SwingLauncher/src/main/java/fr/cakihorse/swinglauncher/Main.java
+++ SwingLauncher/src/main/java/fr/cakihorse/swinglauncher/Main.java
@@ -1,43 +1,75 @@
 package fr.cakihorse.swinglauncher;
 
-
 import fr.cakihorse.swinglauncher.utils.BackgroundPanel;
-
+import fr.litarvan.openauth.microsoft.MicrosoftAuthenticator;
+import javafx.scene.control.Alert;
 
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
 
-        //set new frame
-        String imgUrl="E:\\java\\Launchers\\SwingLauncher\\src\\main\\resources\\background.png";
-        ImageIcon bg = new ImageIcon(imgUrl);
+            String imgUrl = "E:\\java\\Launchers\\SwingLauncher\\src\\main\\resources\\background.png";
+            ImageIcon bg = new ImageIcon(imgUrl);
 
-        String icUrl="E:\\java\\Launchers\\SwingLauncher\\src\\main\\resources\\ic.png";
-        ImageIcon icone = new ImageIcon(icUrl);
+            String icUrl = "E:\\java\\Launchers\\SwingLauncher\\src\\main\\resources\\ic.png";
+            ImageIcon icone = new ImageIcon(icUrl);
 
             JFrame f = new JFrame("Swinglauncher | By Cakihorse");
-            //Set the close operation
             f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            //set size
             f.setSize(1080, 720);
             f.setIconImage(icone.getImage());
+
+            // Créez un panneau personnalisé pour contenir le fond
+            BackgroundPanel backgroundPanel = new BackgroundPanel(bg.getImage());
+
+            // Utilisez un gestionnaire de disposition pour le panneau principal
+            JPanel mainPanel = new JPanel(new BorderLayout());
+
+            // Créez le bouton Microsoft
+            JButton msButton = new JButton("Microsoft");
+            msButton.setBounds(500, 600, 100, 50);
+
+            msButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //TODO : WebView microsoft
+                    authenticateMS();
+                }
+                public void authenticateMS() {
+                    MicrosoftAuthenticator authenticator = new MicrosoftAuthenticator();
+                    authenticator.loginWithAsyncWebview().whenComplete((response, error) -> {
+                        if (error != null) {
+
+                            Alert alert = new Alert(Alert.AlertType.ERROR);
+                            alert.setTitle("Erreur");
+                            alert.setContentText(error.getMessage());
+                            alert.show();
+                            return;
+                        }
+
+                    });
+                }
+
+            });
+
+
+
+
+
+            // Ajoutez le bouton au panneau principal
+            mainPanel.add(msButton, BorderLayout.SOUTH);
+
+            // Ajoutez le panneau de fond et le panneau principal à la fenêtre
+            f.getContentPane().add(backgroundPanel);
+            f.getContentPane().add(mainPanel, BorderLayout.SOUTH);
+
+            f.pack();
             f.setVisible(true);
-
-            //URL de l'image
-
-
-            //Création de JLable avec un alignement gauche
-            JLabel jlabel = new JLabel(bg, JLabel.CENTER);
-
-            //ajouter les deux JLabel a JFrame
-            f.getContentPane().add(jlabel);
-            f.validate();
-
-
+        });
     }
-
-    }
-
+}
--- SwingLauncher/src/main/java/fr/cakihorse/swinglauncher/utils/BackgroundPanel.java
+++ SwingLauncher/src/main/java/fr/cakihorse/swinglauncher/utils/BackgroundPanel.java
@@ -1,21 +1,21 @@
 package fr.cakihorse.swinglauncher.utils;
 
+import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
+import java.awt.Image;
 
 public class BackgroundPanel extends JPanel {
-    private ImageIcon backgroundImage;
+    private Image backgroundImage;
 
-    public BackgroundPanel(String imagePath) {
-        backgroundImage = new ImageIcon(imagePath);
-        setPreferredSize(new Dimension(backgroundImage.getIconWidth(), backgroundImage.getIconHeight()));
+    public BackgroundPanel(Image image) {
+        backgroundImage = image;
+        setPreferredSize(new Dimension(backgroundImage.getWidth(null), backgroundImage.getHeight(null)));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(backgroundImage.getImage(), 0, 0, this);
+        g.drawImage(backgroundImage, 0, 0, this);
     }
 }
