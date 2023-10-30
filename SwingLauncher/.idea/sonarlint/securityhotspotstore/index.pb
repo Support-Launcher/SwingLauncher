275966987035e5bc78b94429548f0f7b90e74fd5
Cakihorse <91017164+cakihorse@users.noreply.github.com>
2023-10-30 22:04:20 +0100

    New Version !
    SwingLauncher : V2 🛠

--- SwingLauncher/.idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,3 +0,0 @@
-
-t
-Dsrc/main/java/fr/cakihorse/swinglauncher/utils/threads/MsThread.java,3\f\3f23ef435376be5cf4fb07ea14b5a983dad2a066
--- jdabot/src/main/java/fr/cakihorse/utils/CommandExecutor.java
+++
@@ -1,8 +0,0 @@
-package fr.cakihorse.utils;
-
-import org.javacord.api.event.message.MessageCreateEvent;
-
-public interface CommandExecutor {
-
-    void run(MessageCreateEvent e, Command command, String[] args);
-}
